242b9cb8cbffb7dceba9e518f8118bd1010292e1
Abhishek <abhishekkr8709@gmail.com>
2025-08-02 01:10:30 +0530

    vercel insights or analytics add

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -5,12 +5,16 @@ import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "./store/store.js";
 import { Toaster } from "./components/ui/toaster.jsx";
+import { SpeedInsights } from "@vercel/speed-insights/react";
+import { Analytics } from "@vercel/analytics/react";
 
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <Provider store={store}>
       <App />
       <Toaster />
+      <SpeedInsights />
+      <Analytics />
     </Provider>
   </BrowserRouter>
 );
